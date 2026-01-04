e15ca1e8ac42845d393819410185e3e1ce944b44
Andrei Roque <roqueandrei0316@gmail.com>
2026-01-04 22:12:29 +0800

    Add a function that removes/deletes a target value from the ArrayList (my own implementation)

--- ArrayList/Arraylist.java
+++ ArrayList/Arraylist.java
@@ -2,6 +2,20 @@ import java.util.ArrayList;
 
 public class Arraylist{
 
+
+  // This function assumes that there's only 1 target (no dupiclate)
+  // I also pass in the target value instead of the index 
+  // Because it would be better for my own sanity.
+  public static void delFromArrayList(int target, ArrayList<Integer> list){
+    for(int i = 0; i < list.size(); i++){
+      if(list.get(i) == target){
+        list.remove(i);
+        break;
+      }
+    }
+  }
+  
+
   public static void main(String[] args){
 
     ArrayList<Integer> nums = new ArrayList<>();
@@ -10,7 +24,6 @@ public class Arraylist{
     nums.add(2);
     nums.add(4);
     nums.add(3);
-
 
     // Printing the values within the ArrayList
     System.out.print("Original: ");
