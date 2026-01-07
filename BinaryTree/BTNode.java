b4a4fd5944a134e7c45c9ab6ed2ded9528b2cf2e
Andrei Roque <roqueandrei0316@gmail.com>
2026-01-07 20:53:58 +0800

    Add recursive insert functionality in BTNode

--- BinaryTree/BTNode.java
+++ BinaryTree/BTNode.java
@@ -6,6 +6,27 @@ public class BTNode{
 
   public BTNode(int val){
     this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+
+
+  public void insert(BTNode n){
+
+    if(n.val <= this.val){
+      if(this.left == null){
+        this.left = n;
+      }else{
+        this.left.insert(n);
+      }
+    }else{
+      if(this.right == null){
+        this.right = n;
+      }else{
+        this.right.insert(n);
+      }
+    }
+
   }
 
 }
--- BinaryTree/Main.java
+++ BinaryTree/Main.java
@@ -2,7 +2,14 @@ public class Main{
 
   public static void main(String[] args){
 
-    
+    BTNode root = new BTNode(5);
+
+    root.insert(new BTNode(2));
+    root.insert(new BTNode(1));
+    root.insert(new BTNode(3));
+    root.insert(new BTNode(8));
+    root.insert(new BTNode(7));
+    root.insert(new BTNode(9));
 
   }
 
