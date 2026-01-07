ea8d4116b2205ee85bbfbbf15f4d3d7aa107dd22
Andrei Roque <roqueandrei0316@gmail.com>
2026-01-07 20:59:32 +0800

    Add inorder traversal function in main

--- BinaryTree/Main.java
+++ BinaryTree/Main.java
@@ -1,5 +1,8 @@
+import java.util.List;
+
 public class Main{
 
+  // Funtion to find the height of the binary tree.
   public static int findHeight(BTNode root){
     if(root == null) return -1;
 
@@ -7,6 +10,28 @@ public class Main{
     int rightHeight = findHeight(root.right);
 
     return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  // Helper function to perform inorder traversal and
+  // populate the 2D matrix
+  public static void inorder(BTNode root, int row, int col, int height, List<List<String>> ans){
+    if(root == null) return;
+
+    // Calculate offset for child position
+    int offset = (int) Math.pow(2, height - row - 1);
+
+    // Traverse the left subtree
+    if(root.left != null){
+      inorder(root.left, row + 1, col - offset, height, ans);
+    }
+
+    // Place the current node's vale in the matrix
+    ans.get(row).set(col, String.valueOf(root.val));
+
+    // Traverse the right subtree
+    if(root.right != null){
+      inorder(root.right, row + 1, col + offset, height, ans);
+    }
   }
 
   public static void main(String[] args){
